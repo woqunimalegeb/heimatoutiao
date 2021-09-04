f08ce054310f16b8252464076dc841e423862baf
cyy <347705996@qq.com>
2021-09-04 09:36:51 +0800

    完成项目架构搭建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id='app'>
 app跟组件
 <i class="toutiao toutiao-shouji"></i>
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+<div class="box"></div>
   </div>
 </template>
 
@@ -24,5 +30,9 @@ export default {
 </script>
 
 <style scoped>
-
+.box{
+  width: 1rem;
+  height: 1rem;
+  background-color: red;
+}
 </style>
